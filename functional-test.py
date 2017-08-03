7c99b76c34f75d63240436b9eea63dda751d9682
homerrowsky <homerrowsky@yahoo.com.mx>
2017-08-02 23:27:23 -0500

    Reescrito implementando unittest y empleando sus funciones de ayuda

--- functional-test.py
+++ functional-test.py
@@ -1,31 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-#Usuario ha escuchado sobre una nueva página con una app de lista de tareas.
-# Usuario va a la url de esta página a checarla
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# Usuario nota que el título de la página y el encabezado dice 'to-do'
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+        self.browser.implicitly_wait(5)
 
-# Se le invita a ingresar una nueva tarea
+        #Usuario ha escuchado sobre una nueva página con una app de lista de tareas.
+        # Usuario va a la url de esta página a checarla
+    def test_can_start_a_list_and_retrieve_it_later(self): 
+        self.browser.get('http://localhost:8000')
 
-# Escribe 'comprar plumas de tzenzontle' en una caja de texto
+        # Usuario nota que el título de la página y el encabezado dice 'to-do'
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Test terminado')
 
-# Cuando presiona Enter, la página se actualiza, y ahora la página enlista
-# '1: Comprar plumas de tzenzontle' como un ítem de lista de tareas
+        # Se le invita a ingresar una nueva tarea
 
-# La caja de texto permanece en la página para agregar otra tarea
-# Usuario ingresa 'usar plumas para penacho lujoso'
+        # Escribe 'comprar plumas de tzenzontle' en una caja de texto
 
-# De nuevo la página se actualiza y ahora muestra ambas tareas
+        # Cuando presiona Enter, la página se actualiza, y ahora la página enlista
+        # '1: Comprar plumas de tzenzontle' como un ítem de lista de tareas
 
-# Usuario se pregunta si el sitio recordará su lista. Se da cuenta que el
-# sitio ha generado un URL única -- hay un texto explicativo para tal efecto
+        # La caja de texto permanece en la página para agregar otra tarea
+        # Usuario ingresa 'usar plumas para penacho lujoso'
 
-# Usuario visita la URL - la lista todavía permanece allí
+        # De nuevo la página se actualiza y ahora muestra ambas tareas
 
-# El usuario, satisfecho, se va a ver videos en youtube
+        # Usuario se pregunta si el sitio recordará su lista. Se da cuenta que el
+        # sitio ha generado un URL única -- hay un texto explicativo para tal efecto
 
-browser.quit()
+        # Usuario visita la URL - la lista todavía permanece allí
+
+        # El usuario, satisfecho, se va a ver videos en youtube
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
